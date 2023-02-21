1cb3152c2c4ceb1857ac820ac53371de5bc4305f
PIROTESS\david <cilmeron@gmail.com>
2023-02-21 11:15:53 +0100

    Initial commit of set up files

---
+++ src/Test.cpp
@@ -0,0 +1,10 @@
+#include "Test.h"
+#include <stdio.h>
+
+namespace LegendaryBeetroot
+{
+	void Print()
+	{
+		printf("Wecleom to LegendaryBeetRoot Engine!\n");
+	}
+}
---
+++ src/Test.h
@@ -0,0 +1,6 @@
+#pragma once
+
+namespace LegendaryBeetroot
+{
+	__declspec(dllexport) void Print();
+}
